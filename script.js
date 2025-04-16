18766a2e4bf20106a7566f6f329773f05f8b78a3
Jacob Lewis-Shores <jacoblewisshores@gmail.com>
2025-04-16 18:07:27 +0100

    Change icon to toggle menu

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ const navLinks = document.querySelector(".nav-links");
 
 menuIcon.onclick = () => {
   navLinks.classList.toggle("active");
+
+  if (window.innerWidth <= 600) {
+    menuIcon.classList.toggle("fa-bars");
+    menuIcon.classList.toggle("fa-xmark");
+  }
 };
